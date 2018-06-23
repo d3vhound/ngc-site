43f204b636eaf0b288afccc42d1d3958afd8e459
Devion Villegas <devion.villegas@ttu.edu>
2018-06-23 11:28:45 -0500

    chatio test

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import HeroAB from '../components/Hero'
-
+import ChatIO from 'react-chatio'
 import Layout from '../components/layout'
 
 const IndexPage = () => (
   <Layout>
     <HeroAB />
+    <ChatIO license={9881965} />
   </Layout>
 )
 
