5c755150b0bca2b8259464fbd711e162628abe91
Devion Villegas <devion.villegas@ttu.edu>
2018-06-23 11:42:58 -0500

    chat io fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,32 @@
-import React from 'react'
+import React, { Component } from 'react'
 import HeroAB from '../components/Hero'
 import ChatIO from 'react-chatio'
 import Layout from '../components/layout'
 
-const IndexPage = () => (
-  <Layout>
-    <HeroAB />
-    <ChatIO license={9881965} />
-  </Layout>
-)
+class IndexPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+      appIsMounted: false 
+    };
+  }
+
+  componentDidMount() {
+    this.setState({
+      appIsMounted: true
+    })
+  }
+
+  
+  render () {
+    const chat = this.state.appIsMounted
+    return (
+    <Layout>
+      <HeroAB />
+      { chat ? ( <ChatIO license={9881965} /> ) : null }
+    </Layout>
+    )
+  }
+}
 
 export default IndexPage
