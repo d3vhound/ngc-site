dffa6755ef0cd8240d9164538682f6c17acac7a3
Devion Villegas <devion.villegas@ttu.edu>
2018-06-23 20:12:51 -0500

    fix window bug

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,34 @@
-import React from 'react'
+import React, { Component } from 'react'
 import HeroAB from '../components/Hero'
 import InfoSection from '../components/InfoSection'
 import ChatIO from 'react-chatio'
 import Layout from '../components/layout'
 
-const IndexPage = () => ( 
-  <Layout>
-    <HeroAB />
-    <InfoSection />
-    <ChatIO license={9881965} />
-  </Layout>
-)
+class IndexPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+      appIsMounted: false 
+    };
+  }
+
+  componentDidMount() {
+    this.setState({
+      appIsMounted: false
+    })
+  }
+
+  
+  render () {
+    const chat = this.state.appIsMounted
+    return (
+    <Layout>
+      <HeroAB />
+      <InfoSection />
+      {chat ? <ChatIO license={9881965} />  : null}
+    </Layout>
+    )
+  }
+}
 
 export default IndexPage
