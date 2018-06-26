e43c96e094c7e2f76d7da9b3d84416811d88b9ec
Devion Villegas <devion.villegas@ttu.edu>
2018-06-26 02:21:50 -0500

    update

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,8 @@ module.exports = {
   },
   plugins: [
     'gatsby-plugin-react-helmet',
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
     'gatsby-plugin-sass',
     'gatsby-transformer-remark',
     {
