e2b83cfd0da0f8acee7aabdf106fd575ecbd51da
Devion Villegas <devion.villegas@ttu.edu>
2018-07-17 17:19:58 -0500

    update

---
+++ src/pages/mr-pink-story.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import SubHero from '../components/SubHero'
+
+import Layout from '../components/layout'
+
+const MrPink = () => (
+	<Layout>
+		<SubHero title="Mr. Pink" subtitle="" />
+		<section className="section">
+			<div className="container">
+				<div className="columns">
+					<div className="column">
+						<h1>Once upon a time...</h1>
+						
+						<p>
+							NextGen Code Company was founded in Lubbock Texas by a group of experienced freelance coders.
+							After years of building relationships within the tech community, the founding team decided to combine
+							their talents and join forces to create NextGen. NextGen’s team has now expanded and grown into an
+							efficient and highly productive workforce that can provide solutions most businesses can only dream up.
+							With a heavy focus on technology related consulting the team doesn’t just understand the code, they
+							understand the problems that they are solving.
+							NextGen has four core founding members. These members all come from different backgrounds and
+							educations which makes the team exceptionally powerful. By combining all their different styles of
+							thinking, they are able to unfold the most powerful solutions possible. Acting as a think tank, the team is
+							able to develop uniquely creative solutions inspiring NextGen to reach outside of the box. Aided by the
+							unbreakable team chemistry and unmatched trust, the team is incredibly capable of providing long-term
+							solutions for a better world.
+							NextGen has taken on several complex projects for their partners, ranging from graphic design,
+							websites, web/mobile apps, UI/UX design, internal business solutions and many more. Combining
+							technology and experience, NextGen believes any dream a partner has can be turned it into reality with
+							the right knowledge and code. The founding members have managed several of their own businesses
+							with success in the past, but they created NextGen to share those discovered solutions from past
+							experiences with all current and future partners. NextGen has the educational and professional
+							qualifications to critically analyze your “wants” and “whys” to produce the absolute best product that
+							you desire.
+							NextGen labels success from producing products like no other. They pride themselves on leaving no
+							stone unturned and perfecting the small overlooked details to overwhelm their partners with pure
+							excellence. NextGen labels success from the relationships they build with their partners. NextGen aims
+							to have their partners walk away from a presentation knowing that they have chosen the right trusted
+							team to help them build their dream into a reality. NextGen labels success by the value their partners
+							receive once the services or products are integrated in their business. Producing and deploying the right
+							product or service properly to their partners for maximum return is NextGen’s goal.
+						</p>
+
+						
+					</div>
+				</div>
+			</div>
+		</section>
+	</Layout>
+)
+
+export default MrPink
