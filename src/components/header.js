0ad74bce953200ca2082e19ac260d821cb7831bd
Devion Villegas <devion.villegas@ttu.edu>
2018-07-18 13:50:19 -0500

    update

--- src/components/header.js
+++ src/components/header.js
@@ -3,7 +3,7 @@ import { Link } from './Link'
 // import USAFlag from '../assets/images/USA_Flag.svg'
 
 
-const Header = ({ siteTitle }) => {
+const Header = () => {
   return (
 		<div className='ng-version-header-container'>
 			<div className="ng-version-badge">
